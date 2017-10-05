07db1f8ef4150053a327f3caa12d2bb65b5dba68
ronney221 <ronneydo1@gmail.com>
2017-10-05 14:22:19 -0700

    fixed puppies code

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -7,7 +7,7 @@ my.name <- "Ronney"
 # Assign your height to a variable `my.height`
 my.height <- 68
 # Create a variable `puppies` equal to the number of puppies you'd like to have
-puppies == 1
+puppies <- 1
 # Create a variable `puppy.price`, which is how expensive you think a puppy is
 puppy.price <- 1500
 # Create a variable `total.cost` that has the total cost of all of your puppies
@@ -16,4 +16,5 @@ total.cost <- 1500
 too.expensive <- total.cost > 1000
 # Create a variable `max.puppies`, which is the nuber of puppies you can afford for $1K.
 max.puppies <- 1000 / total.cost
+
 
